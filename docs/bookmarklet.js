ab13d109361b7e4ecac06f7652f0e693f8770dfc
ケルパニ＠猫 <32389989+kerupani129s@users.noreply.github.com>
2022-04-01 13:38:41 +0900

    update: ブックマークレットのキャッシュ対策変更 (前のキャッシュ対策がキャッシュされていても問題なし)

--- docs/bookmarklet.js
+++ docs/bookmarklet.js
@@ -1,7 +1,6 @@
-(() => {
+(d => {
 	// キャッシュ対策
-	const d = new Date();
-	const s = document.createElement('script');
-	s.src = 'https://kerupani129s.github.io/popn-score-tool/loadScore.js?' + [d.getFullYear(), d.getMonth() + 1, d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds()].map((x, i) => ('' + x).padStart(i ? 2 : 4, '0')).join('');
-	document.head.appendChild(s);
-})();
+	const s = d.createElement('script');
+	s.src = 'https://kerupani129s.github.io/popn-score-tool/loadScore.js?timestamp=' + Date.now() + 'ms';
+	d.head.appendChild(s);
+})(document);
