dbe7e5e27bc47ad45f406edbfbdbf850322521d5
ケルパニ＠猫 <32389989+kerupani129s@users.noreply.github.com>
2022-04-03 19:02:58 +0900

    update: ブックマークレートを全体的にリファクタリング。実行画面改善

--- docs/loadScore.js
+++ docs/loadScore.js
@@ -1,286 +1,303 @@
-(() => {
+(async () => {
 
 	const DEBUG = true;
 
-	// エラーチェック
-	if ( document.domain !== 'p.eagate.573.jp' ) {
-		document.body.innerHTML = '<a href="https://p.eagate.573.jp/">https://p.eagate.573.jp/</a> 上で実行してください<br>';
-		return;
-	}
+	const POPN_RESULTS_URL = 'https://p.eagate.573.jp/game/popn/riddles/playdata/mu_top.html';
 
-	if ( ! ea_common_template.userstatus.state.login ) {
-		document.body.innerHTML = 'ログインしてください<br>';
-		return;
-	}
+	// ウェブサイトを確認
+	const validateWebsite = () => {
 
-	if ( ! ea_common_template.userstatus.state.subscription ) {
-		document.body.innerHTML = 'e-amusement 有料サービスへの加入が必要です<br>';
-		return;
-	}
+		if ( document.domain !== 'p.eagate.573.jp' ) {
+			throw new Error('https://p.eagate.573.jp/ 上で実行してください');
+		}
 
-	if ( ! ea_common_template.userstatus.state.eapass ) {
-		document.body.innerHTML = '参照中の e-amusement pass がありません<br>';
-		return;
-	}
+		if ( ! ea_common_template.userstatus.state.login ) {
+			throw new Error('ログインしてください');
+		}
 
-	if ( ! ea_common_template.userstatus.state.playdata ) {
-		document.body.innerHTML = 'プレーデータがありません<br>';
+		if ( ! ea_common_template.userstatus.state.subscription ) {
+			throw new Error('e-amusement 有料サービスへの加入が必要です');
+		}
+
+		if ( ! ea_common_template.userstatus.state.eapass ) {
+			throw new Error('参照中の e-amusement pass がありません');
+		}
+
+		if ( ! ea_common_template.userstatus.state.playdata ) {
+			throw new Error('プレーデータがありません');
+		}
+
+	};
+
+	try {
+		validateWebsite();
+	} catch (error) {
+		document.body.innerHTML = '<div style="padding: 12px; font-size: 16px; line-height: 1.5; background-color: #fff; color: #000;">' +
+			'<p style="font-weight: bold;">ポップンスコアツール</p>' +
+			'<p style="color: #f00;">' + error.message + '</p>' +
+			'</div>';
+
 		return;
 	}
 
 	// 重複起動チェック
-	if ( window.BOOKMARKLET_TOOL_POPN ) {
-		document.body.insertAdjacentHTML('beforeend', '既に実行済みです<br>');
+	// 
+	// メモ: ウェブサイトの確認の後に行う
+	const preventMultipleStarts = () => {
+
+		if ( window.BOOKMARKLET_TOOL_POPN ) {
+			throw new Error('既に実行済みです');
+		}
+
+		window.BOOKMARKLET_TOOL_POPN = true;
+
+	};
+
+	try {
+		preventMultipleStarts();
+	} catch (error) {
+		const errorStatus = document.getElementById('error-status');
+		errorStatus.innerHTML = error.message;
 		return;
 	}
 
-	window.BOOKMARKLET_TOOL_POPN = true;
+	// 
+	const loadResults = (() => {
 
-	/**
-	 * 頭文字リスト
-	 * 
-	 * 日本語は Shift-JIS の半角カタカナを URL エンコードしたもの
-	 */
-	const initials = [
-		'%B1', '%B2', '%B3', '%B4', '%B5',
-		'%B6', '%B7', '%B8', '%B9', '%BA',
-		'%BB', '%BC', '%BD', '%BE', '%BF',
-		'%C0', '%C1', '%C2', '%C3', '%C4',
-		'%C5', '%C6', '%C7', '%C8', '%C9',
-		'%CA', '%CB', '%CC', '%CD', '%CE',
-		'%CF', '%D0', '%D1', '%D2', '%D3',
-		'%D4', '%D5', '%D6',
-		'%D7', '%D8', '%D9', '%DA', '%DB',
-		'%DC', '%A6', '%DD',
-		'A', 'B', 'C', 'D', 'E', 'F', 'G',
-		'H', 'I', 'J', 'K', 'L', 'M', 'N',
-		'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-		'V', 'W', 'X', 'Y', 'Z',
-		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-		'*', '@'
-	];
-
-	/**
-	 * Blob を Shift-JIS の文字列とみなして UTF-8 文字列にパース
-	 */
-	const parseBlob = blob => {
-		return new Promise(resolve => {
+		const readAsText = (blob, encoding = null) => new Promise((resolve, reject) => {
 			const reader = new FileReader();
-			reader.onload = () => { resolve(reader.result) };
-			reader.onerror = () => { reader.abort(); reject(reader.error); };
-			reader.readAsText(blob, 'shift-jis');
+			reader.onload = () => resolve(reader.result);
+			reader.onerror = () => reject(reader.error);
+			reader.readAsText(blob, encoding);
 		});
-	};
 
-	/**
-	 * HTML 文字列を DOM にパース
-	 */
-	const parseHTML = string => new DOMParser().parseFromString(string, 'text/html');
-
-	/**
-	 * ページをパース
-	 */
-	const parsePage = dom => {
-
-		const array = [];
-
-		// 取得
-		const table = dom.getElementsByClassName('mu_list_table').item(0);
-
-		if ( table !== null ) {
-			const elements = table.getElementsByTagName('li');
-
-			for (let i = 1; i < elements.length; i++) { // 見出しの行があるので i = 1 から
-				const row = parseRow(elements.item(i));
-				array.push(row);
-			}
-		}
-
-		return array;
-
-	};
-
-	/**
-	 * 行パース
-	 */
-	const parseRow = element => {
-
-		// 曲名・ジャンル名・ID
-		const music = element.getElementsByClassName('col_music').item(0);
-
-		const music_link  = music.getElementsByTagName('a').item(0);
-		const music_title = music_link.textContent;
-		const music_id    = parseQuery(music_link.search.slice(1)).no;
-
-		const music_genre = music.getElementsByTagName('div').item(0).textContent;
-
-		// メダル・スコア
-		const column_easy   = element.getElementsByClassName('col_5').item(0);
-		const column_normal = element.getElementsByClassName('col_normal').item(0);
-		const column_hyper  = element.getElementsByClassName('col_hyper').item(0);
-		const column_ex     = element.getElementsByClassName('col_ex').item(0);
-
-		const img_easy   = column_easy.getElementsByTagName('img');
-		const img_normal = column_normal.getElementsByTagName('img');
-		const img_hyper  = column_hyper.getElementsByTagName('img');
-		const img_ex     = column_ex.getElementsByTagName('img');
-
-		const medal_easy   = getMedal(img_easy.item(0).src);
-		const medal_normal = getMedal(img_normal.item(0).src);
-		const medal_hyper  = getMedal(img_hyper.item(0).src);
-		const medal_ex     = getMedal(img_ex.item(0).src);
-
-		const rank_easy   = getRank(img_easy.item(1).src);
-		const rank_normal = getRank(img_normal.item(1).src);
-		const rank_hyper  = getRank(img_hyper.item(1).src);
-		const rank_ex     = getRank(img_ex.item(1).src);
-
-		const score_easy   = column_easy.textContent;
-		const score_normal = column_normal.textContent;
-		const score_hyper  = column_hyper.textContent;
-		const score_ex     = column_ex.textContent;
+		const domParser = new DOMParser();
+		const parseHTML = html => domParser.parseFromString(html, 'text/html');
 
 		// 
-		const row = {
-			'genre': music_genre, 'title': music_title, 'id': music_id,
-			'results': {
-				'easy'  : {'medal': medal_easy  , 'rank': rank_easy  , 'score': score_easy  },
-				'normal': {'medal': medal_normal, 'rank': rank_normal, 'score': score_normal},
-				'hyper' : {'medal': medal_hyper , 'rank': rank_hyper , 'score': score_hyper },
-				'ex'    : {'medal': medal_ex    , 'rank': rank_ex    , 'score': score_ex    }
-			}
+		const parseQuery = query => new URLSearchParams(query);
+
+		const fileNameRegEx = /([^\/]*)$/;
+		const getMedal = src => fileNameRegEx.exec(src)[1];
+		const getRank = getMedal;
+
+		const parseRow = element => {
+
+			// 曲名・ジャンル名・ID
+			const music = element.getElementsByClassName('col_music')[0];
+
+			const music_link  = music.getElementsByTagName('a')[0];
+			const music_title = music_link.textContent;
+			const music_id    = parseQuery(music_link.search).get('no'); // メモ: 楽曲 ID は変更される場合がある
+
+			const music_genre = music.getElementsByTagName('div')[0].textContent;
+
+			// メダル・スコア
+			const column_easy   = element.getElementsByClassName('col_5')[0];
+			const column_normal = element.getElementsByClassName('col_normal')[0];
+			const column_hyper  = element.getElementsByClassName('col_hyper')[0];
+			const column_ex     = element.getElementsByClassName('col_ex')[0];
+
+			const img_easy   = column_easy.getElementsByTagName('img');
+			const img_normal = column_normal.getElementsByTagName('img');
+			const img_hyper  = column_hyper.getElementsByTagName('img');
+			const img_ex     = column_ex.getElementsByTagName('img');
+
+			const medal_easy   = getMedal(img_easy[0].src);
+			const medal_normal = getMedal(img_normal[0].src);
+			const medal_hyper  = getMedal(img_hyper[0].src);
+			const medal_ex     = getMedal(img_ex[0].src);
+
+			const rank_easy   = getRank(img_easy[1].src);
+			const rank_normal = getRank(img_normal[1].src);
+			const rank_hyper  = getRank(img_hyper[1].src);
+			const rank_ex     = getRank(img_ex[1].src);
+
+			const score_easy   = column_easy.textContent;
+			const score_normal = column_normal.textContent;
+			const score_hyper  = column_hyper.textContent;
+			const score_ex     = column_ex.textContent;
+
+			// 
+			const row = {
+				'genre': music_genre, 'title': music_title, 'id': music_id,
+				'results': {
+					'easy'  : { 'medal': medal_easy  , 'rank': rank_easy  , 'score': score_easy   },
+					'normal': { 'medal': medal_normal, 'rank': rank_normal, 'score': score_normal },
+					'hyper' : { 'medal': medal_hyper , 'rank': rank_hyper , 'score': score_hyper  },
+					'ex'    : { 'medal': medal_ex    , 'rank': rank_ex    , 'score': score_ex     },
+				},
+			};
+
+			return row;
+
 		};
 
-		return row;
+		// 
+		const loadResults = async (results, initial, page) => {
 
-	};
+			const url = POPN_RESULTS_URL + '?page=' + page + '&genre=' + initial;
 
-	/**
-	 * クエリ文字列をオブジェクトにパース
-	 */
-	const parseQuery = query => {
-		const pairs = query.split('&');
-		const object = {};
-		pairs.forEach(pairString => {
-			const pair = pairString.split('=');
-			const key   = decodeURIComponent(pair[0]);
-			const value = decodeURIComponent(pair[1]);
-			object[key] = value;
-		});
-		return object;
-	};
+			// 
+			const response = await fetch(url, { credentials: 'include' });
+			const blob = await response.blob(); // メモ: Shift-JIS で扱うために Blob を経由
+			const html = await readAsText(blob, 'shift-jis');
+			const doc = parseHTML(html);
 
-	/**
-	 * メダル・ランク画像パスパース
-	 */
-	const getMedal = src => /([^\/]+)$/.exec(src)[1];
-	const getRank = getMedal;
+			// 
+			const table = doc.getElementsByClassName('mu_list_table')[0];
 
-	/**
-	 * 頭文字ごとに読み込み
-	 */
-	const loadScoreOfInitial = initial => loadScoreOfPage(initial, 0);
-
-	const loadScoreOfPage = async (initial, page) => {
-
-		const url = 'https://p.eagate.573.jp/game/popn/riddles/playdata/mu_top.html?page=' + page + '&genre=' + initial;
-
-		const response = await fetch(url, {credentials: 'include'});
-		const blob = await response.blob();
-		const str = await parseBlob(blob);
-
-		const array = parsePage(parseHTML(str));
-
-		if ( array.length === 20 ) {
-			const arrayNext = await loadScoreOfPage(initial, page + 1);
-			return array.concat(arrayNext);
-		} else {
-			return array;
-		}
-
-	};
-
-	/**
-	 * ファイル保存ダイアログ表示
-	 * 
-	 * Chrome, Firefox, Edge 対応
-	 */
-	const showSaveFileDialog = (data, name, type) => {
-
-		const a = document.createElement('a');
-		a.href = URL.createObjectURL(new Blob([data], {'type': type}));
-		a.download = name;
-
-		a.style.display = 'none';
-		document.body.appendChild(a);
-		a.click();
-		document.body.removeChild(a);
-
-	};
-
-	/**
-	 * メイン
-	 */
-	const main = async () => {
-
-		// 取得開始
-		document.body.innerHTML = '取得中...<br>';
-
-		// 取得
-		if ( DEBUG ) { // テスト
-
-			const array = await loadScoreOfInitial(initials[0]);
-
-			// 結果保存
-			if ( array.length === 0 ) {
-				document.body.insertAdjacentHTML('beforeend', '取得失敗<br>');
-				return;
+			// メモ: あるジャンル名の存在しないページではテーブルが存在するが、
+			//       ジャンル名自体が存在しない場合はテーブルが存在しないため、
+			//       テーブルが見つからない場合でもエラー扱いにしない
+			if ( ! table ) {
+				return 0;
 			}
 
-			document.body.insertAdjacentHTML('beforeend', '取得終了 (' + array.length + ' 曲)<br>');
-			const json = JSON.stringify(array, null, '    ');
-			showSaveFileDialog(json, 'score.json', 'application/json')
+			// 
+			const elements = table.getElementsByTagName('li');
 
-		} else { // 本番
+			if ( 0 === elements.length ) {
+				throw new Error('li が見つかりませんでした');
+			}
 
-			const arrayRaw = [];
+			for (let i = 1; i < elements.length; i++) { // 見出しの行があるので i = 1 から
+				const element = elements[i];
+				const row = parseRow(element);
+				results.push(row);
+			}
 
-			for (let i = 0; i < initials.length; i++) {
+			return elements.length - 1;
 
-				const initial = initials[i];
+		};
 
-				const arrayCurrent = await loadScoreOfInitial(initial);
+		return loadResults;
 
-				// メモ: concat() は新しい配列のインスタンスを生成し、push() は引数展開をすると引数個数上限に
-				//       達する可能性ああるため、ここでは単純に for 文で要素を追加する
-				for (const row of arrayCurrent) {
-					arrayRaw.push(row);
+	})();
+
+	await (async () => {
+
+		// 頭文字リスト
+		// 
+		// 日本語は Shift-JIS の半角カタカナを URL エンコードしたもの
+		const INITIALS = [
+			'%B1', '%B2', '%B3', '%B4', '%B5',
+			'%B6', '%B7', '%B8', '%B9', '%BA',
+			'%BB', '%BC', '%BD', '%BE', '%BF',
+			'%C0', '%C1', '%C2', '%C3', '%C4',
+			'%C5', '%C6', '%C7', '%C8', '%C9',
+			'%CA', '%CB', '%CC', '%CD', '%CE',
+			'%CF', '%D0', '%D1', '%D2', '%D3',
+			'%D4', '%D5', '%D6',
+			'%D7', '%D8', '%D9', '%DA', '%DB',
+			'%DC', '%A6', '%DD',
+			'A', 'B', 'C', 'D', 'E', 'F', 'G',
+			'H', 'I', 'J', 'K', 'L', 'M', 'N',
+			'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+			'V', 'W', 'X', 'Y', 'Z',
+			'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+			'*', '@',
+		];
+
+		document.body.innerHTML = '<div style="padding: 12px; font-size: 16px; line-height: 1.5; background-color: #fff; color: #000;">' +
+			'<p style="font-weight: bold;">ポップンスコアツール</p>' +
+			'<p id="status"></p>' +
+			'<p id="error-status" style="color: #f00;"></p>' +
+			'<p>' +
+			'<label for="progress">進捗:</label> ' +
+			'<progress id="progress" max="' + INITIALS.length + '" value="0"></progress>' +
+			' <span id="percentage">0</span>%' +
+			'</p>' +
+			'</div>';
+
+		const status = document.getElementById('status');
+		const errorStatus = document.getElementById('error-status');
+		const progress = document.getElementById('progress');
+		const percentage = document.getElementById('percentage');
+
+		const getResults = async () => {
+
+			if ( DEBUG ) { // テスト
+
+				const results = [];
+
+				await loadResults(results, INITIALS[0], 0);
+
+				// 進行状況表示 (疑似的に最大値)
+				progress.value = INITIALS.length; // max = INITIALS.length
+				percentage.textContent = Math.round(100 * progress.value / progress.max);
+
+				return results;
+
+			} else { // 本番
+
+				const resultsRaw = [];
+
+				let totalPages = 0; // 確認用
+
+				for (let i = 0; i < INITIALS.length; i++) {
+
+					const initial = INITIALS[i];
+
+					for (let page = 0; ; page++) {
+						const count = await loadResults(resultsRaw, initial, page);
+						if ( 0 !== count ) totalPages++; // 確認用
+						if ( 20 !== count ) break;
+					}
+
+					// 進行状況表示 (等速で進行しない)
+					progress.value = i + 1; // max = INITIALS.length
+					percentage.textContent = Math.round(100 * progress.value / progress.max);
+
 				}
 
-				// 進行状況の目安表示 (均一に進行するわけでない)
-				document.body.insertAdjacentHTML('beforeend', '' + (i + 1) + ' / ' + initials.length + '<br>');
+				console.log('The total number of pages: ' + totalPages); // 確認用
 
-				const element = document.documentElement;
-				window.scroll(0, element.scrollHeight - element.clientHeight);
+				// 重複削除
+				const results = resultsRaw.filter((x, i, a) => a.findIndex(x2 => x.id === x2.id) === i);
+
+				return results;
 
 			}
 
-			const array = arrayRaw.filter((x, i, a) => a.findIndex(x2 => x.id === x2.id) === i); // 重複削除
+		};
 
-			// 結果保存
-			if ( array.length === 0 ) {
-				document.body.insertAdjacentHTML('beforeend', '取得失敗<br>');
-				return;
+		// 
+		const main = async () => {
+
+			try {
+
+				// 取得開始
+				status.innerHTML = '取得中 ...';
+
+				const results = await getResults();
+
+				if ( results.length === 0 ) {
+					throw new Error('取得結果が空です');
+				}
+
+				// データ変換
+				const json = JSON.stringify(results, null, '    ');
+				const blob = new Blob([json], { 'type': 'application/json' });
+				const url = URL.createObjectURL(blob); // メモ: 本ツールの性質上、URL.revokeObjectURL() しない
+
+				// 取得終了
+				status.innerHTML = '取得終了 (' + results.length + ' 曲)<br>' +
+					'<a id="download-link" href="' + url + '" download="score.json">スコアデータ (JSON 形式) ダウンロード</a><br>' +
+					'※自動でダウンロードが開始されない場合や誤ってダウンロードをキャンセルした場合等は上記のリンクからダウンロードできます。';
+
+				document.getElementById('download-link').click();
+
+			} catch (error) {
+				status.innerHTML = '取得中断';
+				errorStatus.innerHTML = '取得失敗';
+				console.error(error);
 			}
 
-			document.body.insertAdjacentHTML('beforeend', '取得終了 (' + array.length + ' 曲)<br>');
-			const json = JSON.stringify(array, null, '    ');
-			showSaveFileDialog(json, 'score.json', 'application/json');
+		};
 
-		}
+		await main();
 
-	};
-
-	main();
+	})();
 
 })();
